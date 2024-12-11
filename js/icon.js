e36cab570308c9448d1dc190e69ee2374e854bf5
Erfan Ebrahimkhani <se.erfanebrahimkhani2@gmail.com>
2024-12-12 00:53:04 +0330

    Initial commit

---
+++ js/icon.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+  <head>
+    <title>air uni</title>
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+    <h2 class="head" class="mb-12 ">خوش آمدید!</h2>  
+          </div> </div></div>
+          
+    <h5 class="text"><i class="bi bi-airplane-engines-fill"></i> نرم افزار دارایی دانشگاه علوم و فنون هوایی شهید ستاری<i class="bi bi-airplane-engines-fill"></i> </h5>
+    <br>
+    </header>
+</body>
+</html>
